d5a257140c51c7ab92baef6268760f7e8298ed14
leandrocirilojs <48395196+leandrocirilojs@users.noreply.github.com>
2024-11-17 17:54:48 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const apiKeys = [
 ];
 let currentKeyIndex = 0;
 let page = 1;
-let currentQuery = 'mundo';
+let currentQuery = 'tecnologia;
 
 // Função para obter a API key atual
 function getCurrentApiKey() {
